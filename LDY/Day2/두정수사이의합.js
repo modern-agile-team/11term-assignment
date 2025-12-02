e8e43c9ecbab776249972dc3c17079668e62a669
임동영 <imdong207@gmail.com>
2025-12-02 14:42:36 +0900

    2일차 과제

---
+++ LDY/Day2/두정수사이의합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+  if (a === b) return a;
+  if (a > b) [a, b] = [b, a];
+
+  let result = 0;
+  for (let i = a; i <= b; i++) {
+    result += i;
+  }
+
+  return result;
+}
