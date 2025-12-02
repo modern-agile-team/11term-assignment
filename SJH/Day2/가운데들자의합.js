df639f44d3c14d250dcd6e9125946c0af213c6ca
seojunho <jayhoan24@gmail.com>
2025-12-02 20:11:20 +0900

    리뷰반영

--- SJH/Day2/가운데들자의합.js
+++ SJH/Day2/가운데들자의합.js
@@ -1,8 +1,9 @@
 function solution(s) {
-    var answer = '';
-    if (s.length % 2 === 0)
-    {
-        return s.substr((s.length/2)-1,2)
-    } else
-        return s[Math.floor(s.length/2)]
+    let answer = '';
+
+    if (s.length % 2 === 0){
+        return s.substr( (s.length / 2) -1, 2)
+    } else {
+        return s[Math.floor(s.length / 2)]
+    }
 }
