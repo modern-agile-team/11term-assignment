a0df90d5f6555cb76fce274e306ec7a82a94dd9a
Nova021114 <lee3882jh@gmail.com>
2025-12-09 17:14:33 +0900

    이재혁 1일차 2일차 과제

---
+++ LJH/Day1/문자열 내 p와 y의 개수.js
@@ -0,0 +1,6 @@
+function solution(s) {
+    s = s.toLowerCase();
+    const pCount = [...s].filter(ch => ch === 'p').length;
+    const yCount = [...s].filter(ch => ch === 'y').length;
+    return pCount === yCount;
+}
