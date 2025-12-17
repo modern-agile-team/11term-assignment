70f555e07fc6f036b314b16058860e5ab3323aec
seojunho <jayhoan24@gmail.com>
2025-12-17 17:42:26 +0900

    서준호 7일차 과제

---
+++ SJH/Day7/체육복.js
@@ -0,0 +1,24 @@
+function solution(n, lost, reserve) {
+    lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
+    let participation = n - lost.length;
+    const LostSet = new Set(lost);
+    for (let i = 0; i < reserve.length; i++) {
+        if (LostSet.has(reserve[i])) {
+            participation++;
+            LostSet.delete(reserve[i]);
+            reserve[i] = -1;
+        }
+    }
+    for (let i = 0; i < reserve.length; i++) {
+        if (reserve[i] === -1) continue;
+        if (LostSet.has(reserve[i] - 1)) {
+            participation++;
+            LostSet.delete(reserve[i] - 1);
+        } else if (LostSet.has(reserve[i] + 1)) {
+            participation++;
+            LostSet.delete(reserve[i] + 1);
+        }
+    }
+    return participation;
+}
