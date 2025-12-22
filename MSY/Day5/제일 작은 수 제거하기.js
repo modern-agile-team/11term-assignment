4432498de55ba98828c6e16126774d9036e6efa7
myoungngng <msy411@naver.com>
2025-12-22 18:04:02 +0900

    5일차 과제

---
+++ MSY/Day5/제일 작은 수 제거하기.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+  if (arr.length === 1) {
+    return [-1];
+  }
+
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) { 
+      min = arr[i];
+    }
+  }
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== min) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
