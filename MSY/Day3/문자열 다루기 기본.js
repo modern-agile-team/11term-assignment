c994e7271d8b8b4859665a67d793a0e538ec0eb6
myoungngng <msy411@naver.com>
2025-12-22 10:20:42 +0900

    3일차 피드백 반영

--- MSY/Day3/문자열 다루기 기본.js
+++ MSY/Day3/문자열 다루기 기본.js
@@ -1,17 +1,3 @@
 function solution(s) {
-    const length = s.length;
-
-    if (length !== 4 && length !== 6) {
-        return false;
-    }
-
-    for (let i = 0; i < length; i++) {
-        const onlyNumber = s[i];
-
-        if (onlyNumber < '0' || onlyNumber > '9') {
-            return false;
-        }
-    }
-
-    return true;
+    return /^(\d{4}|\d{6})$/.test(s);
 }
