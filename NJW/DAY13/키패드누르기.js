0b5ea5f4168f8f09c729bcb6af8eb99e7ea26820
wjddns0122 <jwoon2004@gmail.com>
2025-12-24 17:29:34 +0900

    13,14일차 피드백 반영 및 계산기 완성

--- NJW/DAY13/키패드누르기.js
+++ NJW/DAY13/키패드누르기.js
@@ -1,12 +1,12 @@
 function solution(numbers, hand) {
     var answer = "";
-    let left_pos = 10;
-    let right_pos = 12;
+    let left_pos = 10; // '*'를 10으로 간주
+    let right_pos = 12; // '#'를 12로 간주
 
     for (let i = 0; i < numbers.length; i++) {
         let num = numbers[i];
+        if (num === 0) num = 11; // 0을 11로 치환하여 계산
 
-        if (num === 0) num = 11;
         if (num === 1 || num === 4 || num === 7) {
             answer += "L";
             left_pos = num;
@@ -14,34 +14,31 @@ function solution(numbers, hand) {
             answer += "R";
             right_pos = num;
         } else {
-            if (num === 0) {
-                num = 11;
+            // 2, 5, 8, 11(0)번 처리
+            // 좌표를 쓰지 않고 거리 계산하는 공식
+            let dist_l =
+                Math.floor(Math.abs(num - left_pos) / 3) +
+                (Math.abs(num - left_pos) % 3);
+            let dist_r =
+                Math.floor(Math.abs(num - right_pos) / 3) +
+                (Math.abs(num - right_pos) % 3);
 
-                let dist_l =
-                    parseInt(Math.abs(num - left_pos) / 3) +
-                    parseInt(Math.abs(num - left_pos) % 3);
-                let dist_r =
-                    parseInt(Math.abs(num - right_pos) / 3) +
-                    parseInt(Math.abs(num - right_pos) % 3);
-
-                if (dist_l < dist_r) {
-                    answer += "L";
-                    left_pos = num;
-                } else if (dist_r < dist_l) {
+            if (dist_l < dist_r) {
+                answer += "L";
+                left_pos = num;
+            } else if (dist_r < dist_l) {
+                answer += "R";
+                right_pos = num;
+            } else {
+                if (hand === "right") {
                     answer += "R";
                     right_pos = num;
                 } else {
-                    if (hand === "right") {
-                        answer += "R";
-                        right_pos = num;
-                    } else {
-                        answer += "L";
-                        left_pos = num;
-                    }
+                    answer += "L";
+                    left_pos = num;
                 }
             }
         }
     }
-
     return answer;
 }
