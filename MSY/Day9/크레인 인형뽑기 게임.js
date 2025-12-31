28d6de58db01d4dba5f2cf180442a4f7a3564dbb
myoungngng <msy411@naver.com>
2025-12-31 16:53:32 +0900

    9일차 과제 피드백 반영

--- MSY/Day9/크레인 인형뽑기 게임.js
+++ MSY/Day9/크레인 인형뽑기 게임.js
@@ -1,25 +1,30 @@
-function solution(board, moves) {
-    moves = moves.map(mv => mv - 1);
-    
-    let stack = [];
-    let cnt = 0;
-    
-    for (let i of moves) {
-        for (let row of board) {
-            if (row[i] != 0) {
-                let doll = row[i];
-                row[i] = 0;
-                
-                if (stack.length > 0 && stack[stack.length - 1] === doll) {
-                    stack.pop();
-                    cnt += 2;
-                } else {
-                    stack.push(doll);
-                }
-                
-                break;
-            }
+function pickUpDoll(board, column) {
+    for (let row = 0; row < board.length; row++) {
+        if (board[row][column] !== 0) {
+            const doll = board[row][column];
+            board[row][column] = 0;
+            return doll;
         }
     }
-    return cnt;
+    return null;
+}
+function handleBasket(basket, doll) {
+    if (!doll) return 0;
+    const last = basket[basket.length - 1];
+    if (last === doll) {
+        basket.pop();
+        return 2;
+    }
+    basket.push(doll);
+    return 0;
+}
+function solution(board, moves) {
+    moves = moves.map(mv => mv - 1);
+    const basket = [];
+    let score = 0;
+    for (const column of moves) {
+        const doll = pickUpDoll(board, column);
+        score += handleBasket(basket, doll);
+    }
+    return score;
 }
