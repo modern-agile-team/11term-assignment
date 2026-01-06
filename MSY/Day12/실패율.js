6289d805e146a04ba778aadfeb0e9207e20750d4
myoungngng <msy411@naver.com>
2026-01-06 11:34:19 +0900

    변수를 상수로 변경

--- MSY/Day12/실패율.js
+++ MSY/Day12/실패율.js
@@ -1,23 +1,22 @@
 function solution(N, stages) {
-  let dic = {};
+  const dic = {};
   let allPlayer = stages.length;
 
-  let stageCount = Array(N + 2).fill(0);
-  for (let s of stages) {
+  const stageCount = Array(N + 2).fill(0);
+  for (const s of stages) {
     stageCount[s]++;
   }
 
   for (let i = 1; i <= N; i++) {
-    let notClearPlayer = stageCount[i];
-    let failRate = allPlayer === 0 ? 0 : notClearPlayer / allPlayer;
+    const notClearPlayer = stageCount[i];
+    const failRate = allPlayer === 0 ? 0 : notClearPlayer / allPlayer;
 
     dic[i] = failRate;
     allPlayer -= notClearPlayer;
   }
 
-  let dicSort = Object.entries(dic).sort((a, b) => b[1] - a[1]);
-  
-  let result = dicSort.map(([stage]) => Number(stage));
+  const dicSort = Object.entries(dic).sort((a, b) => b[1] - a[1]);
+  const result = dicSort.map(([stage]) => Number(stage));
 
   return result;
 }
